e603bbd72148f89ec343ad7023f4f8b46486a038
Rafael Papallas <rpapallas@gmail.com>
2023-06-22 11:52:16 +0100

    Improved build script to ignore dir exists warning

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-mkdir build
+mkdir -p build
 c++ -O3 -Wall -shared -std=c++11 -I/usr/include/eigen3 -I../franka_analytical_ik/Eigen -fPIC $(python3 -m pybind11 --includes) panda_ik_pump.cpp -o build/panda_ik_pump.so
 c++ -O3 -Wall -shared -std=c++11 -I/usr/include/eigen3 -I../franka_analytical_ik/Eigen -fPIC $(python3 -m pybind11 --includes) panda_ik_hand.cpp -o build/panda_ik_hand.so
